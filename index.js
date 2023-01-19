dd33418e1e5f18d49eb50b6462fb7d9b2302bc7e
preethdev <123038428+preethdev@users.noreply.github.com>
2023-01-19 07:03:23 +0530

    initial

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = 4000;
+const quotaconfig = require('./quotaconfig.json');
+
+let books = [];
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/book', (req, res) => {
+    const book = req.body;
+
+    // Output the book to the console for debugging
+    console.log(book);
+    books.push(book);
+
+    res.send('Book is added to the database');
+});
+app.get('/test', (req, res) => {
+    res.send(quotaconfig);
+});
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
